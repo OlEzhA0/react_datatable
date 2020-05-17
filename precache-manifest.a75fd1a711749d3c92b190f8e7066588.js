cb192315784ec4546cdd8cf78dd5624214dad230
Oleg <fasfas7971@gmail.com>
2020-05-17 14:51:35 +0300

    Updates

---
+++ precache-manifest.a75fd1a711749d3c92b190f8e7066588.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda75acd7d3226aa0efccad784c09af7",
+    "url": "/react_datatable/index.html"
+  },
+  {
+    "revision": "53eff1d3a0e4bd04d50c",
+    "url": "/react_datatable/static/css/main.020a646e.chunk.css"
+  },
+  {
+    "revision": "2e7d7182d5e4a2d02b56",
+    "url": "/react_datatable/static/js/2.aed2690c.chunk.js"
+  },
+  {
+    "revision": "53eff1d3a0e4bd04d50c",
+    "url": "/react_datatable/static/js/main.8f359f3e.chunk.js"
+  },
+  {
+    "revision": "cd26d19e335861885027",
+    "url": "/react_datatable/static/js/runtime~main.ce12b3cb.js"
+  }
+]);
